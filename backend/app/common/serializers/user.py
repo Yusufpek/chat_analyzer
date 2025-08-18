d600819cef6668c3c958d8641d0b862610adebf6
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-08-18 14:45:29 +0300

    Add password field to UserSerializer and implement password handling in create and update methods

--- backend/app/common/serializers/user.py
+++ backend/app/common/serializers/user.py
@@ -12,6 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
             "pk",
             "username",
             "email",
+            "password",
             "first_name",
             "last_name",
             "profile_image",
@@ -28,6 +29,22 @@ class UserSerializer(serializers.ModelSerializer):
     def save(self, **kwargs):
         self.instance = super().save(**kwargs)
         return self.instance
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().create(validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class UserLoginSerializer(serializers.Serializer):
