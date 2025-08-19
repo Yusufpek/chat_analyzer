3d93e126c1ce5ebd0b694d6347d437b0b0e90788
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-08-19 17:25:54 +0300

    avatar_url field type updated (url field to char field)

--- backend/app/common/models/connection.py
+++ backend/app/common/models/connection.py
@@ -48,7 +48,7 @@ class Agent(models.Model):
     )
     id = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=255, null=True, blank=True)
-    avatar_url = models.URLField(null=True, blank=True)
+    avatar_url = models.CharField(max_length=127, null=True, blank=True)
 
     def __str__(self):
         return f"{self.connection.connection_type} - {self.id}"
