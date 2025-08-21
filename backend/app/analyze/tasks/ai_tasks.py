234232c3f9bf3201e2924572238181e9952a98e3
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-08-21 15:28:07 +0300

    add ai tasks

---
+++ backend/app/analyze/tasks/ai_tasks.py
@@ -0,0 +1,22 @@
+from celery import shared_task
+from django.core import management
+
+from common.models.log import Log
+
+from datetime import datetime
+
+
+@shared_task
+def get_sentimental_analysis_task():
+    log = Log(task_name="Get Sentimental Analysis", category=Log.Category.PERIODIC)
+    log.save()
+    try:
+        management.call_command("get_sentimental_analysis")
+    except Exception as e:
+        print(e)
+    now = datetime.now()
+    log.complete_task(
+        "Analyzed conversations {}".format(now.strftime("%m/%d/%Y, %H:%M"))
+    )
+    print("log saved :)")
+    return True
