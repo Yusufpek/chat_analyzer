345dd0853c5d2c8b221fedaab86207bfc3980d1b
Yusuf İpek <yusuf55ipek55@gmail.com>
2025-09-01 15:49:38 +0300

    feat: implement EmbeddingService for handling embeddings with OpenAI API

---
+++ backend/app/analyze/utils/embedding_service.py
@@ -0,0 +1,43 @@
+from .ai_service import AIService, EngineType
+
+
+class EmbeddingService(AIService):
+    model = None
+
+    def __init__(self, model: str = "text-embedding-3-large"):
+        super().__init__(engine=EngineType.OPENAI, endpoint="v1/embeddings")
+        self.model = model
+
+    def send_request(self, content):
+        data = {
+            "model": self.model,
+            "input": content,
+            "endcoding_format": "float",
+            "dimensions": 3072,
+        }
+
+        return super().send_request(data)
+
+    def parse_response(self, response):
+        if "data" not in response:
+            return None
+        if "embedding" not in response["data"][0]:
+            return None
+        return response["data"][0]["embedding"]
+
+    def generate_embedding(self, messages):
+        if not messages:
+            raise ValueError("No messages provided for embedding generation.")
+
+        points = []
+        for message in messages:
+            response = self.send_request(message["content"])
+            if response:
+                points.append(
+                    {
+                        "id": message["embedding_id"],
+                        "payload": {"message": message["content"]},
+                        "vector": response,
+                    }
+                )
+        return points
